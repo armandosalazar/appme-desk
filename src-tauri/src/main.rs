0dc0eecd83823e4a5b32bf599206a8368b6d6bc7
Armando Salazar <48894886+armandosalazar@users.noreply.github.com>
2024-07-06 13:46:06 -0600

    build: add diesel and dotenvy dependencies

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,6 +2,7 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 mod store;
+use serde::Deserialize;
 use store::commands;
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
@@ -11,9 +12,23 @@ fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+#[derive(Debug, Deserialize)]
+struct Word {
+    word: String,
+    meaning: String,
+    pronunciation: String,
+}
+
+#[tauri::command]
+fn get_words(word_json: &str) -> String {
+    let word: Word = serde_json::from_str(word_json).unwrap();
+    println!("Word: {:?}", word);
+    String::from("Hello from Rust!")
+}
+
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![greet, get_words])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
